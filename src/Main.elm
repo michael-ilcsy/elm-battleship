0f97960ec5abf05eafd515ce2506a1c90f8af038
michael-ilcsy <michael_ilcsy_git@yahoo.co.jp>
2020-07-20 02:07:03 +0900

    [add] とりあえず試しにshipの型を作ってみた

--- src/Main.elm
+++ src/Main.elm
@@ -21,6 +21,43 @@ type GameBoardState
     = Unknown
 
 
+type alias Coordinate =
+    ( Int, Int )
+
+
+type Direction
+    = Right
+    | Left
+    | Up
+    | Down
+
+
+{-| 船の先端マスを表す型
+-}
+type alias ShipTip =
+    ( Coordinate, Direction )
+
+
+type BattleShip
+    = Battleship Coordinate Coordinate Coordinate Coordinate ShipTip
+
+
+type Cruiser
+    = Cruiser Coordinate Coordinate Coordinate ShipTip
+
+
+type Destroyer
+    = Destroyer Coordinate Coordinate ShipTip
+
+
+type Submarine
+    = Submarine Coordinate ShipTip
+
+
+type ShipData
+    = ShipData BattleShip Cruiser Cruiser Destroyer Destroyer Destroyer Destroyer Submarine Submarine
+
+
 init : ( Model, Cmd Msg )
 init =
     ( { gameBoard = initGameBoard }, Cmd.none )
